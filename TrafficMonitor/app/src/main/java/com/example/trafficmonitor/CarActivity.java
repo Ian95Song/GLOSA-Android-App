90a49acc40d601d843a7b9d4d1002c163f73d7de
Oday <oday.kabha@campus.tu-berlin.de>
2020-12-13 17:03:02 +0100

    Create Splash Screen and Activities (Car, Bicycle and Main) and Adding Action Bar

---
+++ TrafficMonitor/app/src/main/java/com/example/trafficmonitor/CarActivity.java
@@ -0,0 +1,88 @@
+package com.example.trafficmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class CarActivity extends AppCompatActivity {
+
+    /*Timer Functions*/
+    private TextView m_textViewCountDown;
+    private Button m_btnStartSimulation;
+    private CountDownTimer m_countDownTimer;
+    private long m_timeLeftInMillis = 6000;
+
+    private void timer(){
+        m_countDownTimer = new CountDownTimer(m_timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                m_timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                m_timeLeftInMillis = 6000;
+                updateCountDownText();
+                updateTrafficLight();
+            }
+        }.start();
+    }
+    private void updateCountDownText(){
+        int seconds = (int) (m_timeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d", seconds);
+        m_textViewCountDown.setText(timeLeftFormatted);
+    }
+    /*Timer Functions*/
+
+    /*Traffic Light Functions*/
+    private static ImageView m_imageView;
+    private int current_image;
+    int[] images = {R.drawable.red, R.drawable.red_yellow, R.drawable.yellow, R.drawable.green};
+
+    public void updateTrafficLight(){
+        m_imageView = (ImageView)findViewById(R.id.imageView3);
+        current_image++;
+        current_image=current_image % images.length;
+        m_imageView.setImageResource(images[current_image]);
+    }
+    /*Traffic Light Functions*/
+
+    /*GPS Information (Yiyang)*/
+    private static TextView m_gpsInformation;
+
+
+    /*GPS Information*/
+
+    /*Http Client and JSON Parser (Yuanheng)*/
+    //lste[length-1]
+    private static TextView m_JSONObject;
+    /*Http Client and JSON Parser (Yuanheng)*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car);
+
+        m_textViewCountDown = findViewById(R.id.textView5);
+        m_btnStartSimulation = findViewById(R.id.button);
+        m_gpsInformation = findViewById(R.id.textView8);
+        m_JSONObject = findViewById(R.id.textView9);
+
+        m_btnStartSimulation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer();
+            }
+        });
+        updateCountDownText();
+    }
+
+}
--- TrafficMonitor/app/src/main/java/com/example/trafficmonitor/MainActivity.java
+++ TrafficMonitor/app/src/main/java/com/example/trafficmonitor/MainActivity.java
@@ -2,87 +2,44 @@ package com.example.trafficmonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.Locale;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    /*Timer Functions*/
-    private TextView m_textViewCountDown;
-    private Button m_btnStartSimulation;
-    private CountDownTimer m_countDownTimer;
-    private long m_timeLeftInMillis = 6000;
-
-    private void timer(){
-        m_countDownTimer = new CountDownTimer(m_timeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                m_timeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                m_timeLeftInMillis = 6000;
-                updateCountDownText();
-                updateTrafficLight();
-            }
-        }.start();
-    }
-    private void updateCountDownText(){
-        int seconds = (int) (m_timeLeftInMillis / 1000) % 60;
-        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d", seconds);
-        m_textViewCountDown.setText(timeLeftFormatted);
-    }
-    /*Timer Functions*/
-
-    /*Traffic Light Functions*/
-    private static ImageView m_imageView;
-    private int current_image;
-    int[] images = {R.drawable.red, R.drawable.red_yellow, R.drawable.yellow, R.drawable.green};
-
-    public void updateTrafficLight(){
-        m_imageView = (ImageView)findViewById(R.id.imageView3);
-        current_image++;
-        current_image=current_image % images.length;
-        m_imageView.setImageResource(images[current_image]);
-    }
-    /*Traffic Light Functions*/
-
-    /*GPS Information (Yiyang)*/
-    private static TextView m_gpsInformation;
-
-
-    /*GPS Information*/
-
-    /*Http Client and JSON Parser (Yuanheng)*/
-    //lste[length-1]
-    private static TextView m_JSONObject;
-    /*Http Client and JSON Parser (Yuanheng)*/
-
+    ImageButton m_changeToCarActivity;
+    ImageButton m_changeToBicyleActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        m_textViewCountDown = findViewById(R.id.textView5);
-        m_btnStartSimulation = findViewById(R.id.button);
-        m_gpsInformation = findViewById(R.id.textView8);
-        m_JSONObject = findViewById(R.id.textView9);
+        m_changeToCarActivity = (ImageButton) findViewById(R.id.imageButton);
+        m_changeToBicyleActivity = (ImageButton) findViewById(R.id.imageButton2);
 
-        m_btnStartSimulation.setOnClickListener(new View.OnClickListener() {
+        m_changeToCarActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                timer();
+                openCarActivity();
             }
         });
-        updateCountDownText();
+        m_changeToBicyleActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBicyleActivity();
+            }
+        });
     }
 
+    public void openCarActivity(){
+        Intent carIntent = new Intent(MainActivity.this, CarActivity.class);
+        startActivity(carIntent);
+        //finish();
+    }
+    public void openBicyleActivity(){
+        Intent bicyleIntent = new Intent(MainActivity.this, BicycleActivity.class);
+        startActivity(bicyleIntent);
+        //finish();
+    }
 }
